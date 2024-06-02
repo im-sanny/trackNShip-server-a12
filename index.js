7256b8c9b5294a4cfc119523af7fd699509021c9
im-sanny <raselparvezsanny@gmail.com>
2024-06-02 16:41:27 +0600

    basic server created successfully

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+const corsOptions = {
+  origin: ["http://localhost:5173/", "http://localhost:5174/"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from TrackNShip.....!!!");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
