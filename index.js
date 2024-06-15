0cc5855ae262bca6170efc086c1ee3d1ea5de93f
im-sanny <raselparvezsanny@gmail.com>
2024-06-15 17:57:47 +0600

    made some changes and fixed payment related bugs

--- index.js
+++ index.js
@@ -12,36 +12,13 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://tracknship22.web.app",
-    "http://localhost:5174",
+    "https://tracknship22.firebaseapp.com",
   ],
   credentials: true,
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
-// verify token middleware
-const verifyToken = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access ): " });
-  }
-
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access ): " });
-  }
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({ message: "unauthorized access ): " });
-    }
-    req.user = decoded;
-    next();
-  });
-};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -61,15 +38,7 @@ async function run() {
     const reviewCollection = db.collection("reviews");
     const paymentCollection = db.collection("payments");
 
-    // payment related api
-    app.post("/payments", async (req, res) => {
-      const payment = req.body;
-      const paymentResult = await paymentCollection.insertOne(payment);
-      console.log("payment info", payment);
-      res.send(paymentResult);
-    });
-
-    // auth related api
+    // jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -78,37 +47,93 @@ async function run() {
       res.send({ token });
     });
 
-    // Logout (Not necessary without cookies, but kept for any future use)
-    app.get("/logout", async (req, res) => {
-      try {
-        res.send({ success: true });
-        console.log("Logout successful");
-      } catch (err) {
-        res.status(500).send(err);
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
       }
-    });
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
-    // verify admin middleware
     const verifyAdmin = async (req, res, next) => {
-      const user = req.user;
-      const query = { email: user?.email };
-      const result = await usersCollection.findOne(query);
-      if (!result || result.role !== "admin") {
-        return res.status(401).send({ message: "unauthorized access!!" });
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
     };
 
     // verify deliveryman middleware
     const verifyDeliveryman = async (req, res, next) => {
-      const user = req.user;
-      const query = { email: user?.email };
+      console.log("hello");
+      const email = req.decoded.email;
+      const query = { email: email };
       const result = await usersCollection.findOne(query);
+      console.log(result.role);
       if (!result || result.role !== "deliveryman") {
         return res.status(401).send({ message: "forbidden access" });
       }
       next();
     };
+
+    // Endpoint to fetch top delivery men
+    app.get("/deliverymen", async (req, res) => {
+      try {
+        const deliveryCollection = db.collection("users"); // Assuming delivery men are stored in the "users" collection
+
+        // Fetch users with role "deliveryman" from the database
+        const deliveryMen = await deliveryCollection
+          .find({ role: "deliveryman" })
+          .toArray();
+
+        // Calculate the number of parcels delivered and average ratings for each delivery man
+        const deliveryMenData = await Promise.all(
+          deliveryMen.map(async (deliveryMan) => {
+            const { _id, name } = deliveryMan;
+            const parcelCount = await bookParcelCollection.countDocuments({
+              deliveryManID: _id,
+            });
+            const reviews = await reviewCollection
+              .find({ deliveryManID: _id })
+              .toArray();
+            const totalRating = reviews.reduce(
+              (acc, review) => acc + review.rating,
+              0
+            );
+            const averageRating =
+              reviews.length > 0 ? totalRating / reviews.length : 0;
+            return { _id, name, parcelCount, averageRating };
+          })
+        );
+
+        //     // Sort delivery men by the number of parcels delivered and average ratings
+        const topDeliveryMen = deliveryMenData
+          .sort((a, b) => {
+            if (a.parcelCount !== b.parcelCount) {
+              return b.parcelCount - a.parcelCount;
+            }
+            return b.averageRating - a.averageRating;
+          })
+          .slice(0, 3); // Get the top 3 delivery men
+
+        // Send the top delivery men data as response
+        res.json(topDeliveryMen);
+      } catch (error) {
+        console.error("Error fetching top delivery men:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
     // save all user data in db
     app.put("/user", async (req, res) => {
@@ -130,7 +155,7 @@ async function run() {
         }
       }
 
-      // save user for first time
+      // save user for  first time
       const options = { upsert: true };
       const updateDoc = {
         $set: {
@@ -156,19 +181,24 @@ async function run() {
     });
 
     // update a user role
-    app.patch("/user/update/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = req.body;
-      const query = { email };
-      const updateDoc = {
-        $set: { ...user, timeStamp: Date.now() },
-      };
-      const result = await usersCollection.updateOne(query, updateDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/user/update/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const query = { email };
+        const updateDoc = {
+          $set: { ...user, timeStamp: Date.now() },
+        };
+        const result = await usersCollection.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
 
     // save book parcel data in db
-    app.post("/bookParcel", async (req, res) => {
+    app.post("/bookParcel",verifyToken, async (req, res) => {
       const bookParcelData = req.body;
       const result = await bookParcelCollection.insertOne(bookParcelData);
       res.send(result);
@@ -197,20 +227,25 @@ async function run() {
     });
 
     // Update booking status and assign delivery man
-    app.post("/updateBooking/:id", async (req, res) => {
-      const id = req.params.id;
-      const { deliveryManID, approximateDeliveryDate } = req.body;
-      const query = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          status: "On The Way",
-          deliveryManID: new ObjectId(deliveryManID),
-          approximateDeliveryDate: new Date(approximateDeliveryDate),
-        },
-      };
-      const result = await bookParcelCollection.updateOne(query, updateDoc);
-      res.send(result);
-    });
+    app.post(
+      "/updateBooking/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const { deliveryManID, approximateDeliveryDate } = req.body;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            status: "On The Way",
+            deliveryManID: new ObjectId(deliveryManID),
+            approximateDeliveryDate: new Date(approximateDeliveryDate),
+          },
+        };
+        const result = await bookParcelCollection.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
 
     // Cancel Parcel
     app.patch("/cancelParcel/:id", async (req, res) => {
@@ -229,7 +264,7 @@ async function run() {
             const deliveryManUpdateDoc = {
               $inc: { deliveredParcelCount: -1 }, // Reduce deliveredParcelCount by 1
             };
-            await deliveryCollection.updateOne(
+            await bookParcelCollection.updateOne(
               deliveryManQuery,
               deliveryManUpdateDoc
             );
@@ -243,7 +278,7 @@ async function run() {
     });
 
     // Deliver Parcel
-    app.patch("/deliverParcel/:id", async (req, res) => {
+    app.patch("/deliverParcel/:id",verifyToken, async (req, res) => {
       const { id } = req.params;
       const query = { _id: new ObjectId(id) };
       const updateDoc = {
@@ -259,7 +294,7 @@ async function run() {
             const deliveryManUpdateDoc = {
               $inc: { deliveredParcelCount: 1 }, // Increase deliveredParcelCount by 1
             };
-            await deliveryCollection.updateOne(
+            await bookParcelCollection.updateOne(
               deliveryManQuery,
               deliveryManUpdateDoc
             );
@@ -272,45 +307,120 @@ async function run() {
       }
     });
 
-    // delivery man all assigned parcel
-    app.get("/assignedParcels/:email", async (req, res) => {
-      const email = req.params.email;
-      const deliveryMan = await usersCollection.findOne({ email });
-      if (!deliveryMan || deliveryMan.role !== "deliveryman") {
-        return res.status(404).send({ message: "deliveryman not found" });
-      }
-
-      const query = {
-        deliveryManID: deliveryMan._id,
-        status: "On The Way",
-      };
-      const result = await bookParcelCollection.find(query).toArray();
-      res.send(result);
-    });
-
-    // Add a review
+    // save review data in db
     app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewCollection.insertOne(review);
+      const reviewData = req.body;
+      const result = await reviewCollection.insertOne(reviewData);
       res.send(result);
     });
 
-    // Get all reviews
+    // get all review
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
 
-    await client.connect();
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // delete booking upon canceling the booking
+    app.delete("/cancelParcel/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookParcelCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // get data of single id before update
+    app.get("/getUpdate/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookParcelCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update a booking data
+    app.patch("/getUpdate/:id", async (req, res) => {
+      const id = req.params.id;
+      const parcelData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: parcelData,
+      };
+      const result = await bookParcelCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // update after successfull payment
+    app.patch("/getUpdateStatus/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: { price: "paid" }, // Update the price field to "paid"
+        };
+        const result = await bookParcelCollection.updateOne(query, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating booking data:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log("Amount in cents:", amount);
+
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error("Error creating payment intent:", error);
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    // create-payment-intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = req.body.price;
+      const priceInCent = parseFloat(price) * 100;
+      console.log("priceInCent", priceInCent);
+      if (!price || priceInCent < 1) return;
+      // generate clientSecret
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      // send client secret as response
+      res.send({ clientSecret: client_secret });
+    });
+
+    // payment related api
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      console.log("payment info", payment);
+      res.send(paymentResult);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.listen(port, () => {
-  console.log(`track-n-ship app listening on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello from TrackNShip.....!!!");
 });
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
